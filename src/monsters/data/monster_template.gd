12d46a871cf738d6354b74bc110baa81e74259a8
Red <red.kalab@vacuumlabs.com>
2021-10-26 00:18:51 +0200

    base monster structure, bat

---
+++ src/monsters/data/monster_template.gd
@@ -0,0 +1,11 @@
+class_name MonsterTemplate
+extends Resource;
+
+
+export(String) var id;
+export(int) var health = 10;
+export(float) var movement_speed = 20;
+
+export(SpriteFrames) var texture;
+
+export(String) var attack_id = "single";
