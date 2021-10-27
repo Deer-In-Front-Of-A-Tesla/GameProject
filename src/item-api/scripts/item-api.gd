2335231d98748412f18401ac83d255a868099cc8
EmilChoparinov <emilchoparinov@gmail.com>
2021-10-27 21:54:59 +0300

    documentation

--- src/item-api/scripts/item-api.gd
+++ src/item-api/scripts/item-api.gd
@@ -1,3 +1,13 @@
+"""
+Authors: Emil Choparinov,
+Created On: 27/10/2021
+Description::
+	Use this small framework to manage item creation logic.
+	
+	Note: This API does not manage deletion, your code will have to handle this
+	yourself.
+"""
+
 class_name ItemAPI
 extends Node;
 
@@ -5,7 +15,10 @@ export(Resource) var _entity = _entity as mainplayer;
 
 signal item_applied;
 
-func _process(delta):
+# DONT EXTEND
+# If you do you will break the framework unless you extend it with these three
+# lines as well
+func _process(_delta):
 	if(_should_use_item()):
 		_apply_item();
 		emit_signal("item_applied");
--- src/items/max-hp/scripts/max-hp-item.gd
+++ src/items/max-hp/scripts/max-hp-item.gd
@@ -1,3 +1,10 @@
+"""
+Authors: Emil Choparinov,
+Created On: 27/10/2021
+Description::
+	Max HP Item Implementiaton.
+"""
+
 class_name MaxHPItem
 extends ItemAPI;
 
@@ -7,13 +14,14 @@ var _collision_occured = false;
 
 func _ready():
 	_item.connect("body_entered", self, "_on_collide");
-	
+
 func _should_use_item() -> bool:
 	if(_collision_occured):
 		return true;
 	return false;
 
 func _apply_item() -> void:
+	# once we apply, we delete ourselves
 	_entity.hp = _entity.maxhp;
 	_item.queue_free();
 
