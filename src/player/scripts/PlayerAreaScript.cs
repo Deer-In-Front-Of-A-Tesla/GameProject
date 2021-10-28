2dd2c9919f52fcc64cb07cd8697bb96373ecb334
EmilChoparinov <emilchoparinov@gmail.com>
2021-10-28 12:34:17 +0200

    script fix?

--- src/player/scripts/PlayerAreaScript.cs
+++ src/player/scripts/PlayerAreaScript.cs
@@ -3,24 +3,24 @@ using System;
 
 public class PlayerAreaScript : Node
 {
-    [Signal] public delegate void GetHit(int damage);
-    
-    public override void _Ready()
-    {
-        this.Connect("GetHit", GetNode("/root/Player/MainPlayerBody"), "TakeDamage");
-        
-        this.Connect("body_entered", this, nameof(DetectCollition));
-    }
+	[Signal] public delegate void GetHit(int damage);
+	
+	public override void _Ready()
+	{
+		this.Connect("GetHit", GetNode("/root/Player/MainPlayerBody"), "TakeDamage");
+		
+		this.Connect("body_entered", this, nameof(DetectCollition));
+	}
 
-    public void DetectCollition(PhysicsBody2D node)
-    {
-        Console.WriteLine(node.CollisionLayer);
-        if (node.CollisionLayer == 4)
-        {
-            Console.WriteLine("Got Hit");
-            EmitSignal(nameof(GetHit), node.Get("damage"));
-            node.Call("AnnihilateNode");
-        }
-        
-    }
+	public void DetectCollition(PhysicsBody2D node)
+	{
+		Console.WriteLine(node.CollisionLayer);
+		if (node.CollisionLayer == 4)
+		{
+			Console.WriteLine("Got Hit");
+			EmitSignal(nameof(GetHit), node.Get("damage"));
+			node.Call("AnnihilateNode");
+		}
+		
+	}
 }
